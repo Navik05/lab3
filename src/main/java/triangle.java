9c5b02cd03b224d574c37bfe4bb2be9dec3dd70d
naviko <naviko05@mail.ru>
2023-12-16 17:53:22 +0700

    all

--- src/main/java/triangle.java
+++ src/main/java/triangle.java
@@ -3,21 +3,21 @@ public class triangle extends figure{
     private int view;
     public triangle(double[] sides){
         System.arraycopy(sides, 0, this.sides, 0, 3);
-        view=0;
-        area=0;
-        perimeter=0;
+        view = 0;
+        area = 0;
+        perimeter = 0;
     }
     public int getView() {
         return view;
     }
     public void workTriangle(){
-        double p2;
+        double p;
         perimeter = sides[0] + sides[1] + sides[2];
-        p2 = perimeter / 2;
-        area =Math.sqrt(p2 * (p2 - sides[0]) * (p2 - sides[1]) * (p2 - sides[2]));
+        p = perimeter / 2;
+        area = Math.sqrt(p * (p - sides[0]) * (p - sides[1]) * (p - sides[2]));
         if (sides[0] == sides[1] && sides[1] == sides[2])
             view = 1;
-        else if (sides[0] == sides[1] || sides[1] == sides[2])
+        else if (sides[0] == sides[1] || sides[1] == sides[2]|| sides[0] == sides[2])
             view = 2;
         else view = 3;
     }
