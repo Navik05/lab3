e5d36f7b8e8c49e71141c79ded66990e56ac4e02
Naviko05 <Naviko05@mail.ru>
2026-03-31 23:41:15 +0700

    add game class

---
+++ tic_tac_toe/game.py
@@ -0,0 +1,10 @@
+from tic_tac_toe.board import Board
+
+class Game:
+    def __init__(self, player1, player2):
+        self.board = Board()
+        self.player1 = player1
+        self.player2 = player2
+        self.current_player = player1
+        self.is_over = False
+        self.winner = None
