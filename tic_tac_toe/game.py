72d9de010d3be525fd71618236d8aec93483037f
Naviko05 <Naviko05@mail.ru>
2026-03-31 23:56:03 +0700

    add switch player test method

--- tic_tac_toe/game.py
+++ tic_tac_toe/game.py
@@ -8,3 +8,6 @@ class Game:
         self.current_player = player1
         self.is_over = False
         self.winner = None
+
+    def switch_player(self):
+        self.current_player = self.player2 if self.current_player == self.player1 else self.player1
