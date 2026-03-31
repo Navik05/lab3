b39c1522ae830a38fd5b848d96228a0a8ab8f7da
Naviko05 <Naviko05@mail.ru>
2026-03-31 22:19:36 +0700

    add make_move method

--- tic_tac_toe/board.py
+++ tic_tac_toe/board.py
@@ -1,3 +1,9 @@
 class Board:
     def __init__(self):
         self.cells = [[None for _ in range(3)] for _ in range(3)]
+
+    def make_move(self, row, col, player):
+        if self.cells[row][col] is None:
+            self.cells[row][col] = player
+            return True
+        return False
